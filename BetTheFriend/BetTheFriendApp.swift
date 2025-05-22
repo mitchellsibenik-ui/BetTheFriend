5e5ab864af999f0e2777b1f494512389c76a5eb1
Mitch <MitchellSibenik@gmail.com>
2025-05-22 16:03:36 -0400

    Initial Commit

---
+++ BetTheFriend/BetTheFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetTheFriendApp.swift
+//  BetTheFriend
+//
+//  Created by Mitchell Sibenik on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct BetTheFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
